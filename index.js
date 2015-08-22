d07f887b1cc7c9610e984ece87b52950bd74d75a
Alex <a.chepura@gmail.com>
2015-08-22 14:55:41 +0300

    sf.ajax.actions

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ if (!window.hasOwnProperty("sf")){//bind only if  window.sf is empty to avoid co
     window.sf = sf;
 }
 
+require("./lib/core/ajax/actions.js"); //plugin to perform actions from the server
 require("./lib/vendor/formToObject"); //formToObject  for form
 require("./lib/instances/form/Form.js"); //add form
 require("./lib/instances/form/addons/formMessages/spiral"); //add form addon
