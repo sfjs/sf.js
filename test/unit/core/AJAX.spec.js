600119395146b7c03343a31637cfee6d6eb70a62
Dmitry <zmicerboksha@gmail.com>
2016-02-11 01:48:48 +0300

    tests

--- test/unit/core/AJAX.spec.js
+++ test/unit/core/AJAX.spec.js
@@ -2,23 +2,27 @@
 var assert = chai.assert;
 var should = chai.should();
 var expect = chai.expect;
-
+sessionStorage.setItem('sfFlashMessage', JSON.stringify({message:'test', timeout: 1}));
+require("../../../src/index");
 
 describe('AJAX', function () {
     var server,
-        data = [{ "id": 12, "comment": "Hey there" }];
+        data = { "id": 12, "comment": "Hey there"};
     beforeEach(function(){
         server = sinon.fakeServer.create();
         server.respondWith("POST","/test", [200, {"Content-Type":"application/json"}, JSON.stringify(data)]);
     });
 
     it("#AJAX.send()", function (done) {
-        sf.ajax.send({url:"/test", data: {}}).then(function(answer){expect(answer[0].id).to.equals(12); done()});
+        sf.ajax.send({url:"/test", data: {}}).then(function(answer){expect(answer.id).to.equals(12); done()});
         server.respond();
     });
     it("#AJAX.send() error without url", function (done) {
         sf.ajax.send({}).then(function(){},function(error){expect(error).to.equals("You should provide url"); done();});
         server.respond();
+    });
+    it("#sessionStorage sfFlashMessage should be undefined", function () {
+        expect(sessionStorage.getItem('sfFlashMessage')).not.to.exist;
     });
     afterEach(function(){
         server.restore();
