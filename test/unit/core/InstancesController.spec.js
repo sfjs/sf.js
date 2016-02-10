3ff03c1985176055dd09b6ea0e9d2694161e9754
Dmitry <zmicerboksha@gmail.com>
2016-02-10 19:11:07 +0300

    tests

--- test/unit/core/InstancesController.spec.js
+++ test/unit/core/InstancesController.spec.js
@@ -16,7 +16,12 @@ testNode0.classList.add('js-sf-module1-class');
 document.body.appendChild(testNode0);
 var testNode01 = document.createElement('div');
 
-describe('API', function () {
+describe('InstancesController', function () {
+    describe('#InstancesController()', function () {
+        it("it's a function", function () {
+            expect(sf.core.InstancesController).be.a('function');
+        });
+    });
     describe('#sf.registerInstanceType()', function () {
         it("it's a function", function () {
             expect(sf.instancesController.registerInstanceType).be.a('function');
@@ -50,10 +55,37 @@ describe('API', function () {
             expect(inst).be.a('array');
         });
     });
-    describe('#sf.removeInstance()', function () {
+    describe('#sf.instancesController.removeInstance()', function () {
         it("removed instance should be undefined", function () {
             sf.instancesController.removeInstance(Module1.prototype.name, testNode01);
             expect(window.sf.instancesController._storage.instances[Module1.prototype.name][1]).to.be.undefined;
+        });
+    });
+    describe('#sf.instancesController.removeInstance()', function () {
+        it("removed instance should be undefined", function () {
+            sf.instancesController.removeInstance(Module1.prototype.name, testNode01);
+            expect(window.sf.instancesController._storage.instances[Module1.prototype.name][1]).to.be.undefined;
+        });
+    });
+    describe('#sf.instancesController.registerAddon()', function () {
+        it("removed instance should register addon", function () {
+            sf.instancesController.registerAddon({'test':1}, 'module', 'fill', 'test');
+            expect(sf.instancesController._storage.addons.module.fill.test.test).to.equals(1);
+        });
+    });
+    describe('#sf.instancesController.getInstanceAddon()', function () {
+        it("removed instance should return addon", function () {
+            expect(sf.instancesController.getInstanceAddon( 'module', 'fill', 'test')).be.a('object');
+        });
+    });
+    describe('#sf.instancesController.getClasses()', function () {
+        it("removed instance should return array with instances classes names", function () {
+            expect(sf.instancesController.getClasses()).be.a('array');
+        });
+    });
+    describe('#sf.instancesController.getInstanceNameByCssClass()', function () {
+        it("should return string with instance name", function () {
+            expect(sf.instancesController.getInstanceNameByCssClass('js-sf-module1-class')).to.equals('module1');
         });
     });
     //console.log(sf.getInstance('module1', testNode01))
