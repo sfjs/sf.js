9680b3d366c11ed4dd997b4f5608ff3591b868af
Dmitry <zmicerboksha@gmail.com>
2016-02-19 02:24:29 +0300

    AJAX tests

--- test/unit/core/AJAX.spec.js
+++ test/unit/core/AJAX.spec.js
@@ -11,14 +11,34 @@ describe('AJAX', function () {
     beforeEach(function(){
         server = sinon.fakeServer.create();
         server.respondWith("POST","/test", [200, {"Content-Type":"application/json"}, JSON.stringify(data)]);
+        server.respondWith("POST","/test1", [500, {"Content-Type":"application/json"}, JSON.stringify(data)]);
+        server.respondWith("POST","/test2", [999, {"Content-Type":"application/json"}, JSON.stringify(data)]);
+        server.respondWith("POST","/test4", [200, {"Content-Type":"application/json"}, 'not json data']);
+        server.respondWith("POST","/test5", [200,{"Content-Type":"fake"},'']);
     });
 
     it("#AJAX.send()", function (done) {
-        sf.ajax.send({url:"/test", data: {}}).then(function(answer){expect(answer.id).to.equals(12); done()});
+        sf.ajax.send({url:"/test", data: {}, onProgress: function(){}}).then(function(answer){expect(answer.id).to.equals(12); done()});
         server.respond();
     });
     it("#AJAX.send() error without url", function (done) {
         sf.ajax.send({}).then(function(){},function(error){expect(error).to.equals("You should provide url"); done();});
+        server.respond();
+    });
+    it("#AJAX.send() answer with code 500 should call error callback ", function (done) {
+        sf.ajax.send({url:"/test1", data: {}}).then(function(){},function(){done();});
+        server.respond();
+    });
+    it("#AJAX.send() answer with code 999 should call error callback ", function (done) {
+        sf.ajax.send({url:"/test2", data: {}}).then(function(){},function(){done();});
+        server.respond();
+    });
+    it("#AJAX.send() should resolve width not json answer ", function (done) {
+        sf.ajax.send({url:"/test4", data: {}}).then(function(){done()},function(error){done(error)});
+        server.respond();
+    });
+    it("#AJAX.send() should resolve width empty answer ", function (done) {
+        sf.ajax.send({url:"/test5", data: {}}).then(function(){done()},function(error){done(error)});
         server.respond();
     });
     it("#sessionStorage sfFlashMessage should has data", function () {
