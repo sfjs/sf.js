eabd28291bdfa24e6d795902ec25fb07121f8f5f
AndrewK <andrew@spiralscout.com>
2017-07-18 14:37:05 +0300

    Update redirect action to support relative pathes with queries and hashes

--- src/core/events/baseEvents.js
+++ src/core/events/baseEvents.js
@@ -2,7 +2,14 @@ module.exports = function (events) {
     events.on("redirect", function (event) {
         var url = Object.prototype.toString.call(event) === "[object String]" ? event : event.url;
         // http://stackoverflow.com/questions/10687099/how-to-test-if-a-url-string-is-absolute-or-relative
-        self.location[/^(?:[a-z]+:)?\/\//i.test(url) ? 'href' : 'pathname'] = url;
+        var isAbsolute = /^(?:[a-z]+:)?\/\//i.test(url);
+        if (isAbsolute) {
+            self.location.href = url;
+        } else {
+            var origin = window.location.origin ||
+                (window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : ''));
+            self.location.href = origin + ((url.charAt(0) === '/') ? url : ('/' + url)); // Relative path
+        }
     });
 
     events.on('reload', function () {
